72e24d9ef86bf44bf6b4d7c4de7939b1448e5784
AbuArwa001 <khalfanathman12@yahoo.com>
2026-09-11 23:20:44 +0300

    Update API for Bulletins and add lifespan db creation

--- app/main.py
+++ app/main.py
@@ -3,10 +3,23 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.v1 import api_router
 
+from contextlib import asynccontextmanager
+from app.db.session import engine
+from app.models.__init__ import *
+from app.db.base import Base
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Automatically create missing database tables on startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    description="Reimplementation of JMCDonations API using FastAPI"
+    description="Reimplementation of JMCDonations API using FastAPI",
+    lifespan=lifespan
 )
 
 # Set up CORS
